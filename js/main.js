fdfb83f264355b99fe16adaccb147a8fcf3543d2
Saulo Pratti <sppratti@gmail.com>
2013-02-08 02:53:20 -0200

    corrige bug que fazia com que o mapa nao carregasse completamente

--- js/main.js
+++ js/main.js
@@ -117,6 +117,7 @@ MBP.enableActive();
                 attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
                 maxZoom: 18
             }).addTo(mapa);
+            L.Util.requestAnimFrame(mapa.invalidateSize, mapa, !1, mapa._container);
             isMapInitialized = !isMapInitialized;
         }
         $.scroll(0);
