85b29ce5f08dd90c652bc1204ffd58a01aba98ee
Saulo Pratti <sppratti@gmail.com>
2013-02-19 07:14:00 -0300

    alterado o nome/numero da linha do exemplo

--- inc/topbar.php
+++ inc/topbar.php
@@ -1,7 +1,7 @@
 		<header class="headsign line line-<?= $page['line_num']; ?>" id="line-<?= $page['line_num']; ?>">
 			<div class="button search-button"><a href="#go-search" class="search-button" title="Buscar linhas" id="go-search">Buscar</a></div>
 			<div class="info" id="goto-current">
-				<div class="line"><a href="#current-location"><strong class="number"><?= $page['line_num'] ?></strong> <span class="name"><?= $page['line_name']; ?></span></a></div>
+				<div class="line"><a href="#current-location"><strong class="number"><?= $page['line_num'] ?></strong> - <span class="name"><?= $page['line_name']; ?></span></a></div>
 				<div class="status updated">Atualizado agora</div>
 			</div>
 			<div class="button map-button"><a href="#" title="Mostrar mapa da linha" id="toggle-map">Mostrar mapa da linha</a></div>
--- results.php
+++ results.php
@@ -1,14 +1,14 @@
 <?php
 include 'inc/util.php';
-partial( array( 'topbar' ), array( 'line_num' => 121, 'line_name' => 'Mário Cypreste' ) );
+partial( array( 'topbar' ), array( 'line_num' => 164, 'line_name' => 'Forte São João' ) );
 ?>
 		<div id="itinerary" class="itinerary" role="main">
 			<div id="mapa" class="mapinha"></div>
 
 			<ul id="past-points" class="past points">
-				<li class="point turn turn-left" id="p0001">
-					<div class="marker turn-left">Virar à esquerda</div>
-					<div class="detail"><p class="name"><strong>Av. Fernando Ferrari</strong></p></div>
+				<li class="point turn turn-left" id="p0000">
+					<div class="marker">Ponto de partida</div>
+					<div class="detail"><p class="name"><strong>Inicio do trajeto</strong></p></div>
 				</li>
 			</ul>
 
